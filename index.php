35785366055983a5cc5583fc51009e55a95ed974
Heath Burton <rubickdev@gmail.com>
2020-10-09 00:36:30 +1100

    one last try for live heroku!

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
+<?php require_once('private/initialise.php'); ?>
+
 <!DOCTYPE html>
-<html>
-   <head>
-      <title>HTML Meta Tag</title>
-      <meta http-equiv = "refresh" content = "0.00001; url = http://tafepracdphptime.herokuapp.com/public/index.php" />
-   </head> 
-   <body>
-      <p>Redirecting to another URL</p>
-   </body>
-</html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Bazaar ceramics is a major ceramics producer and seller offering unique and local products">
+	<meta name="keywords" content="ceramics,pottery,clay,bazaar ceramics,gallery">
+	<meta name="author" content="Heath Burton">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Bazaar Ceramics Landing Page</title>
+  <link rel="stylesheet" media="all" href="public/styles/main.css">
+</head>
+
+<body> <!-- Beginning of Entire Page content -->
+ 
+  <header> <!-- ### Header area containing the Logo and Navbar ### -->
+      
+  <nav id="navbar">
+    <div id="logo">
+      <a id="logoA" href="public/index.php">
+      <img alt="Image of Bazaar Ceramics logo" id="logoImage" 
+      src="/Bazaar_Ceramics_ss/public/images/logo/bazaar-logo.jpg" />             
+      <span class="titleW">Enter Bazaar Ceramics</span></a>
+    </div>
+
+  </nav>  
+
+  </header> <!-- End of Header Section - NAV -->
+
+<div> Welcome to Prac Task D Demonstration PAGE - click the Logo above to enter the site.</div>
+
+
+
 
 <!-- THIS FILE/PAGE HAS BEEN ADDED ONLY FOR THE PURPOSES OF HOSTING ON HEROKU -->
-<!-- LIVE SITE @ http://localhost/Bazaar_Ceramics_ss/public/ -->
+<!-- LIVE SITE @  -->
