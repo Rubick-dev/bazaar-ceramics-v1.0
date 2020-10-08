8e3ff1c21fb94e1950a737a3d1fcbb0c14ad9789
Heath Burton <rubickdev@gmail.com>
2020-10-08 23:38:56 +1100

    added pointer

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-require('/public/index.php');
-?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>HTML Meta Tag</title>
+      C:\wamp64\apps\Bazaar_Ceramics_ss\public
+      <meta http-equiv = "refresh" content = "0.00001; url = http://localhost/Bazaar_Ceramics_ss/public/" />
+   </head> 
+   <body>
+      <p>Redirecting to another URL</p>
+   </body>
+</html>
+
+<!-- THIS FILE/PAGE HAS BEEN ADDED ONLY FOR THE PURPOSES OF HOSTING ON HEROKU -->
