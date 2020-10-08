c6485755187f70901c99065f978a38e8c5dd1a65
Heath Burton <rubickdev@gmail.com>
2020-10-08 11:39:35 +1100

    the break css refactor rethink point

--- private/shared/header.php
+++ private/shared/header.php
@@ -10,10 +10,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title><?php echo $page_title; ?></title>
-  <link rel="stylesheet" href="styles/home/layout.css">
-  <link rel="stylesheet" href="styles/home/styles.css">
+  <link rel="stylesheet" media="all" href="<?php echo url_for('/layout.css'); ?>" />
+  <link rel="stylesheet" media="all" href="<?php echo url_for('/styles.css'); ?>" />
+  <!-- <link rel="stylesheet" href="styles/home/styles.css">
   <link rel="stylesheet" href="styles/home/mobile.css">
   <link rel="stylesheet" href="styles/home/tablet.css">
-  <link rel="stylesheet" href="styles/home/laptop.css">
+  <link rel="stylesheet" href="styles/home/laptop.css"> -->
   
 </head>
