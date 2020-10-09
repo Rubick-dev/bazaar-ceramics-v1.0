d4711f0a6e82c595c0d227f1aa34236320c0d361
Heath Burton <rubickdev@gmail.com>
2020-10-09 17:26:34 +1100

    updated pathing slashes to forward slashes

--- public/html/faq/faq.php
+++ public/html/faq/faq.php
@@ -1,8 +1,8 @@
 <?php require_once('../../../private/initialise.php'); ?>
 <?php $page_title = 'BC - FAQ'; ?>
-<?php include(SHARED_PATH . '\header.php'); ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
 <link rel="stylesheet" media="all" href="<?php echo url_for('/styles/main.css'); ?>" /> 
-<?php include(SHARED_PATH . '\navmenu.php'); ?>
+<?php include(SHARED_PATH . '/navmenu.php'); ?>
 
 
 
