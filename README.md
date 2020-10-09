7b05934564dfa17df38097fed83512cba2ab32c3
Heath Burton <rubickdev@gmail.com>
2020-10-09 12:50:10 +1100

    Final for TAFE upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# TAFEpracTaskB
-This is a mock website of a Cermaics business and created as part of the TAFE Diploma of Web Development
--- private/initialise.php
+++ private/initialise.php
@@ -1,18 +1,11 @@
 <?php
-
-require_once('functions.php'); 
-
-// Assigning file paths to PHP constants to simplify 
-// __FILE__ returns the current path to this file
-// dirname() returns the path to the parent directory
+require_once('functions.php');
 define("PRIVATE_PATH", dirname(__FILE__));
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", PROJECT_PATH . '\public');
 define("SHARED_PATH", PRIVATE_PATH . '\shared');
-
 // Assign the root URL to a PHP constant
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
-
 ?>
