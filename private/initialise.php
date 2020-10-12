9abcad86879142698ae2d61fea5b1d703f5fc9cd
Heath Burton <rubickdev@gmail.com>
2020-10-13 00:24:44 +1100

    Replaced with refactored code 3rd attempt

--- private/initialise.php
+++ private/initialise.php
@@ -2,8 +2,8 @@
 require_once('functions.php');
 define("PRIVATE_PATH", dirname(__FILE__));
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
-define("PUBLIC_PATH", PROJECT_PATH . '/public');
-define("SHARED_PATH", PRIVATE_PATH . '/shared');
+define("PUBLIC_PATH", PROJECT_PATH . '\public');
+define("SHARED_PATH", PRIVATE_PATH . '\shared');
 // Assign the root URL to a PHP constant
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
